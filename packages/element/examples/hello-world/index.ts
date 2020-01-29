7862022c43495e765ae6cc9b6dd09dc8a8652e36
Rob Wormald <robwormald@gmail.com>
2020-01-29 14:54:29 -0800

    feat(elements): scaffold package

---
+++ packages/element/examples/hello-world/index.ts
@@ -0,0 +1,7 @@
+import {HelloWorld} from './hello-world'
+import {fromComponent} from '@angular/element'
+import {ɵrenderComponent} from '@angular/core'
+
+const HelloWorldElement = fromComponent(HelloWorld);
+
+customElements.define('hello-world', HelloWorldElement);
